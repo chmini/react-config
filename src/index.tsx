2e832a044a93cd89526f998c617641d5d5ac730d
chmini <papa00103@naver.com>
2022-09-09 20:24:44 +0900

    Add Emotion, Update ESLint Config and Prettier Config

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
 
-import App from './App'
+import { createRoot } from 'react-dom/client';
 
-const root = createRoot(document.getElementById('root')!)
+import App from './App';
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
